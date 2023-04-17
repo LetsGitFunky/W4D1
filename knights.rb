ed3bd68dc211d660f30c5c7397d5f2ae13bf63b6
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-17 13:31:30 -0700

    after break

---
+++ knights.rb
@@ -0,0 +1,44 @@
+require_relative "tree_node.rb"
+
+class KnightPathFinder
+
+    def initialize #(position)
+        # @position = position
+        # @grid = Array.new(8) { Array.new(8) }
+        self.root_node = PolyTreeNode.new(starting_pos, parent, )
+        @considered_position = []
+
+    end
+
+    def self.root_node
+
+    end
+
+    def find_path
+
+    end
+
+    def build_move_tree(self.root_node)
+
+    end
+
+    def self.valid_moves(pos)
+        
+    end
+
+    def new_move_positions(pos)
+        KnightPathFinder.valid_moves(pos)
+    end
+
+end
+
+
+    # def get_pos
+    #     gets.chomp
+    # end
+
+    # def valid_pos?(arr)
+
+    # end
+
+current_pos = (3,3)
